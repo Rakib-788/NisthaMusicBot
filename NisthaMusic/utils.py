59f2e1aa2527412458d354419f0344d677e3e76a
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2022-12-27 12:44:01 +0530

    Update utils.py

--- NisthaMusic/utils.py
+++ NisthaMusic/utils.py
@@ -4,11 +4,11 @@ import importlib
 from pathlib import Path
 
 def load_plugins(plugin_name):
-    path = Path(f"Anon/plugins/{plugin_name}.py")
-    name = "Anon.plugins.{}".format(plugin_name)
+    path = Path(f"NisthaMusic/plugins/{plugin_name}.py")
+    name = "NisthaMusic.plugins.{}".format(plugin_name)
     spec = importlib.util.spec_from_file_location(name, path)
     load = importlib.util.module_from_spec(spec)
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
-    sys.modules["Anon.plugins." + plugin_name] = load
-    print("BOT HASH STARTED. " + plugin_name)
+    sys.modules["NisthaMusic.plugins." + plugin_name] = load
+    print("» ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ ||. " + plugin_name)
