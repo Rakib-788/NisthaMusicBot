9179e7ecb0a76dc25f495394e0a9d1df8633484b
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <115222641+Sumit0045@users.noreply.github.com>
2022-12-27 12:44:25 +0530

    Update utils.py

--- NisthaMusic/utils.py
+++ NisthaMusic/utils.py
@@ -11,4 +11,4 @@ def load_plugins(plugin_name):
     load.logger = logging.getLogger(plugin_name)
     spec.loader.exec_module(load)
     sys.modules["NisthaMusic.plugins." + plugin_name] = load
-    print("» ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ ||. " + plugin_name)
+    print("» ʙᴏᴛ ʜᴀs sᴛᴀʀᴛᴇᴅ || " + plugin_name)
